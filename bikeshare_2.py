4e78c41dd4c1e788d0f51b4d3a429e3af87ac786
albagonzalez <albupas@hotmail.com>
2019-03-22 12:45:13 +0000

    feat: Add raw input validation

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -17,13 +17,31 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
+    while True:
+        city = input("Specify a city to analyze (Chicago, New York City or Washington): ")
+        if city.lower() not in ('chicago', 'new york city', 'washington'):
+            print("Not an appropriate city.")
+            continue
+        else:
+            break
 
     # get user input for month (all, january, february, ... , june)
-
+    while True:
+        month = input("Specify a month to analyze (all, January, February, ... June): ")
+        if month.lower() not in ('all', 'january', 'february', 'march', 'april', 'may', 'june'):
+            print("Not an appropriate month.")
+            continue
+        else:
+            break
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
-
+    while True:
+        day = input("Specify a day of week to analyze (all, Monday, Tuesday, ... Sunday): ")
+        if day.lower() not in ('all', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'):
+            print("Not an appropriate day of week.")
+            continue
+        else:
+            break
 
     print('-'*40)
     return city, month, day
@@ -121,12 +139,12 @@ def user_stats(df):
 def main():
     while True:
         city, month, day = get_filters()
-        df = load_data(city, month, day)
+        #df = load_data(city, month, day)
 
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
+        #time_stats(df)
+        #station_stats(df)
+        #trip_duration_stats(df)
+        #user_stats(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
